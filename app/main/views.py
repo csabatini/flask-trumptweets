30c51e243b9a58eeaceb96c57dddbf407dd3e25c
Caleb Sabatini <calebsabatini@gmail.com>
2017-04-27 22:27:43 -0500

    render tags in index template

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from . import main
+import services
 from flask import render_template
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', **services.get_tags())
